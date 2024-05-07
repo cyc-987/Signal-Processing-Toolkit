fcd2abc17402a1273ad49d27adc15fbb835b3c30
cyc-987 <2516803593@qq.com>
2024-05-07 12:06:20 +0800

    add: signals.c

---
+++ signals.c
@@ -0,0 +1,12 @@
+//This file contains some functions to create signals
+
+double uniform(double a, double b, long int *seed)
+//This function generates a random number between a and b
+{
+    double t;
+    *seed = 2045 * (*seed) + 1;
+    *seed = *seed - (*seed / 1048576) * 1048576;
+    t = (double)(*seed) / 1048576.0;
+    t = a + t * (b - a);
+    return t;
+}
