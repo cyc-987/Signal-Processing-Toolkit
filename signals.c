83d73a34de86fd6d02808eb50c6f5e898a5f6061
cyc-987 <2516803593@qq.com>
2024-05-11 10:41:52 +0800

    update: signals

--- signals.c
+++ signals.c
@@ -1,8 +1,8 @@
 //This file contains some functions to create signals
 #include "signals.h"
+#include<math.h>
 
 double uniform(double a, double b, long int *seed)
-//This function generates a random number between a and b
 {
     double t;
     *seed = 2045 * (*seed) + 1;
@@ -10,4 +10,69 @@ double uniform(double a, double b, long int *seed)
     t = (double)(*seed) / 1048576.0;
     t = a + t * (b - a);
     return t;
+}
+
+double gauss(double mean, double sigma, long int *seed)
+{
+    int i;
+    double x, y;
+    for(x=0,i=0;i<12;i++)
+        x += uniform(0,1,seed);
+    x -= 6.0;
+    y = mean + sigma * x;
+    return y;
+}
+
+double exponent(double beta, long int *seed)
+{
+    double t;
+    t = uniform(0,1,seed);
+    t = -beta * log(t);
+    return t;
+}
+
+double laplace(double beta, long int *seed)
+{
+    double u1, u2, x;
+    u1 = uniform(0,1,seed);
+    u2 = uniform(0,1,seed);
+    if(u1 < 0.5)
+        x = -beta * log(1. - u2);
+    else
+        x = beta * log(u2);
+    return x;
+}
+
+int bn(double p, long int *seed)
+{
+    double t;
+    t = uniform(0,1,seed);
+    if(t < p)
+        return 1;
+    else
+        return 0;
+}
+
+int bin(int n, double p, long int *seed)
+{
+    int i;
+    double x;
+    for(x =0.0,i=0;i<n;i++)
+        x += bn(p,seed);
+    return x;
+}
+
+int poisson(double lambda, long int *seed)
+{
+    int i, x;
+    double a, b, u;
+    a = exp(-lambda);
+    i = 0; b = 1.0;
+    do{
+        u = uniform(0,1,seed);
+        b *= u;
+        i++;
+    }while(b >= a);
+    x = i - 1;
+    return x;
 }
--- signals.h
+++ signals.h
@@ -3,5 +3,24 @@
 
 double uniform(double a, double b, long int *seed);
 //This function generates a random number between a and b
-
+//平均分布
+double gauss(double mean, double sigma, long int *seed);
+//This function generates a random number from a Gaussian distribution 
+//with mean mean and standard deviation sigma
+//高斯分布
+double exponent(double beta, long int *seed);
+//This function generates a random number from an exponential distribution
+//指数分布
+double laplace(double beta, long int *seed);
+//This function generates a random number from a Laplace distribution
+//拉普拉斯分布
+int bn(double p, long int *seed);
+//This function generates a random number from a Bernoulli distribution
+//伯努利分布
+int bin(int n, double p, long int *seed);
+//This function generates a random number from a binomial distribution
+//二项分布
+int poisson(double lambda, long int *seed);
+//This function generates a random number from a Poisson distribution
+//泊松分布
 #endif
