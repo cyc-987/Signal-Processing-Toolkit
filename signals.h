11503dd1bc1d5d1325ff30514a843cb9c3c883bb
cyc-987 <2516803593@qq.com>
2024-05-27 22:30:43 +0800

    modified: signals.h adjust positions of funcs and annotations

--- signals.h
+++ signals.h
@@ -2,32 +2,32 @@
 #define _SIGNALS_H_
 
 // basic signals
-double uniform(double a, double b, long int *seed);
+
 //This function generates a random number between a and b
 //平均分布
-double gauss(double mean, double sigma, long int *seed);
+double uniform(double a, double b, long int *seed);
 //This function generates a random number from a Gaussian distribution 
 //with mean mean and standard deviation sigma
 //高斯分布
-double exponent(double beta, long int *seed);
+double gauss(double mean, double sigma, long int *seed);
 //This function generates a random number from an exponential distribution
 //指数分布
-double laplace(double beta, long int *seed);
+double exponent(double beta, long int *seed);
 //This function generates a random number from a Laplace distribution
 //拉普拉斯分布
-int bn(double p, long int *seed);
+double laplace(double beta, long int *seed);
 //This function generates a random number from a Bernoulli distribution
 //伯努利分布
-int bin(int n, double p, long int *seed);
+int bn(double p, long int *seed);
 //This function generates a random number from a binomial distribution
 //二项分布
-int poisson(double lambda, long int *seed);
+int bin(int n, double p, long int *seed);
 //This function generates a random number from a Poisson distribution
 //泊松分布
+int poisson(double lambda, long int *seed);
+
 
 // advanced signals
-void sinwn(double a[], double f[], double ph[], int m, 
-double fs, double snr, long seed, double x[], int n);
 //This function generates a sinusoidal signal with white noise
 //产生含高斯白噪声的正弦组合信号
 //a: 各信号振幅，长度m
@@ -39,5 +39,8 @@ double fs, double snr, long seed, double x[], int n);
 //seed: 随机数种子
 //x: 输出信号
 //n: 信号长度
+void sinwn(double a[], double f[], double ph[], int m, 
+double fs, double snr, long seed, double x[], int n);
+
 
 #endif
